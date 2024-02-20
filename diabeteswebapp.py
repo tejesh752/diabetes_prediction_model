64dabcd0f27aeb0862c7fe65793ba5242086eff0
Tejesh Varma <134191227+tejesh752@users.noreply.github.com>
2024-02-20 09:26:33 +0530

    Update diabeteswebapp.py

--- diabeteswebapp.py
+++ diabeteswebapp.py
@@ -9,7 +9,7 @@ import numpy as  np
 import pickle
 import streamlit as st
 
-loaded_model = pickle.load(open(r"trained_model.sav",'rb'))
+loaded_model = pickle.load(open("trained_model.sav",'rb'))
 
 def diabetes_prediction(input_data):
 
